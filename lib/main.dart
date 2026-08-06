f75d394ac4db2a412bbb2a953a3eac66c3b6f75d
Santiago <santiagogomez1129@gmail.com>
2026-08-06 11:14:05 -0500

    Revert "Conectar proyecto con Firebase (flutterfire configure + firebase_core)"
    This reverts commit dbb7c414bdf85298313a2196c1b811c79e84ffd6.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'data/database.dart';
 import 'screens/rutas_list_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(MyApp(database: AppDatabase()));
 }
 
